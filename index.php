ca590a057e3c649d9d6fee8e2e7c63c389e5699d
linocrie <zaqaryanliana14@gmail.com>
2021-04-07 10:46:20 +0400

    condition changing

--- index.php
+++ index.php
@@ -5,16 +5,18 @@ $parts = explode('/', ltrim($req_uri, '/'));
 
 if (file_exists("Controllers/" . $parts[0] . ".php")) {
     require "Controllers/" . $parts[0] . ".php";
-}
-if (class_exists($parts[0])) {
-    $class = $parts[0];
-    $class_obj = new $class;
-    $method = $parts[1];
-    if (method_exists($class_obj, $method)) {
-        $control_obj->$method();
+    if (class_exists($parts[0])) {
+        $class = $parts[0];
+        $class_obj = new $class;
+        $method = $parts[1];
+        if (method_exists($class_obj, $method)) {
+            $control_obj->$method();
+        } else {
+            echo "Not found";
+        }
     } else {
         echo "Not found";
     }
 } else {
-    echo "Not found";
+    echo 'Not found';
 }
