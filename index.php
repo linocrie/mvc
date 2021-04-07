b603bd2a3b61746b1019c2cebeeb20d2d5c451ec
linocrie <zaqaryanliana14@gmail.com>
2021-04-07 10:41:38 +0400

    creating controller files

--- index.php
+++ index.php
@@ -3,20 +3,18 @@
 $req_uri = $_SERVER['REQUEST_URI'];
 $parts = explode('/', ltrim($req_uri, '/'));
 
-if (file_exists("Controller/" . $parts[0] . ".php")) {
+if (file_exists("Controllers/" . $parts[0] . ".php")) {
     require "Controllers/" . $parts[0] . ".php";
-} else {
-    echo 'Not found';
 }
 if (class_exists($parts[0])) {
     $class = $parts[0];
     $class_obj = new $class;
-} else {
-    echo "Not found";
-}
-$method = $parts[1];
-if (method_exists($class_obj, $method)) {
-    $control_obj->$method();
+    $method = $parts[1];
+    if (method_exists($class_obj, $method)) {
+        $control_obj->$method();
+    } else {
+        echo "Not found";
+    }
 } else {
     echo "Not found";
 }
