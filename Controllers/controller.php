17b7a4e259cd686ab419fa4cbc16996aee801077
linocrie <zaqaryanliana14@gmail.com>
2021-04-12 15:11:39 +0400

    modified

--- Controllers/controller.php
+++ Controllers/controller.php
@@ -4,11 +4,6 @@ namespace Controllers;
 
 class Controller
 {
-    public function test($arg_1, $arg_2)
-    {
-        echo $arg_1 . " " . $arg_2;
-    }
-
     public function method_test()
     {
         echo 'test';
