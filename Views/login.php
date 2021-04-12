5bc945e4b56880cdced776ac21eaadc4c156a4af
linocrie <zaqaryanliana14@gmail.com>
2021-04-13 03:18:04 +0400

    using separately layouts and creating view part

--- Views/login.php
+++ Views/login.php
@@ -1,7 +1,7 @@
 <div class = "container">
     <div class="col-md-6 offset-md-3 bg-light border border-secondary rounded mt-5">
         <h2 class="d-flex justify-content-center mt-2">Login</h2>
-        <form class = "m-5" action = "/auth/login_post" method = "POST">
+        <form class = "m-5" action = "/auth/login" method = "POST">
             <div class="form-group">
                 <label for="email">Email address</label>
                 <input type="email" class="form-control" name = "email" aria-describedby="emailHelp" placeholder="Enter email">
