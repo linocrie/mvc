21d29e170cb60a66a4779b82959b1d3349c4a356
linocrie <zaqaryanliana14@gmail.com>
2021-04-12 15:03:39 +0400

    modified

--- System/Routes.php
+++ System/Routes.php
@@ -8,26 +8,33 @@ class Routes
     {
         if (!empty($path[0])) {
             $cntrl_class = ucfirst($path[0]);
-        }
-        if (file_exists("Controllers/" . DIRECTORY_SEPARATOR . $cntrl_class . ".php")) {
-            $cntrl_class = "Controllers\\" . $cntrl_class;
-            if (class_exists($cntrl_class)) {
-                $class = $cntrl_class;
-                $cntrl_obj = new $class;
-                if (!empty($path[1])) {
-                    $method = $path[1];
-                    if (method_exists($cntrl_obj, $method)) {
-                        $params = array_slice($path, 2);
-                        call_user_func_array(array($cntrl_obj, $method), $params);
+            if (file_exists("Controllers" . DIRECTORY_SEPARATOR . $cntrl_class . ".php")) {
+                $cntrl_class = "Controllers\\" . $cntrl_class;
+                if (class_exists($cntrl_class)) {
+                    $cntrl_obj = new $cntrl_class;
+                    if (!empty($path[1])) {
+                        $method = $path[1];
+                        if (method_exists($cntrl_obj, $method)) {
+                            $params = array_slice($path, 2);
+                            call_user_func_array(array($cntrl_obj, $method), $params);
+                        } else {
+                            echo "404 ERROR";
+                        }
                     } else {
-                        echo "Not found";
+                        if (method_exists($cntrl_obj, "index")) {
+                            $cntrl_obj->index();
+                        } else {
+                            echo "Method not found";
+                        }
                     }
+                } else {
+                    echo "ERROR 404";
                 }
             } else {
-                echo "Not found";
+                echo 'ERROR 404';
             }
         } else {
-            echo 'Not found';
+            //idk
         }
     }
 }
