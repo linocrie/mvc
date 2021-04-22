9dbe5734c14288d13356e512b61e3af3ef0ec2b8
linocrie <zaqaryanliana14@gmail.com>
2021-04-22 15:31:11 +0400

    message sending

--- Public/js/script.js
+++ Public/js/script.js
@@ -20,10 +20,6 @@ $("#send").click(function(event){
                 $("#chatList").append("<span class = 'ml-4 mt-3'>Me</span>")
                 $("#chatList").append("<div class='d-flex my-msg m-3 bg-dark text-light flex-row p-2'><h5 class = 'ml-3 mb-0 bg-dark p-1 text-light' style = 'border-radius: 20px;'>" + response['body'] + "</h5></div>");
                 $("#chatList").append("<span class = 'ml-4 mb-1'>" + response['date'] + "</span>")
-                // $('#chatList').scrollTop($('#chatList').scrollHeight);
-                // $('html, body').animate({
-                //     scrollTop:
-                // })
                 $("#text").val("");
             }
         });
